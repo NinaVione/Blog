5d08d4fbee6318cc20f95b149860058c5b7ec712
Nina Vione <nina-vione@yandex.ru>
2014-11-19 00:20:32 +0300

    beta v1.2
    done: menu, content, gallery, pagination, search

--- bin/main/webapp/js/search.js
+++ bin/main/webapp/js/search.js
@@ -1,40 +1,48 @@
-var TRange = null;
-
-function findString(str) {
-	if (parseInt(navigator.appVersion) < 4)
-		return;
-	var strFound;
-	if (window.find) {
-
-		// CODE FOR BROWSERS THAT SUPPORT window.find
-
-		strFound = self.find(str);
-		if (!strFound) {
-			strFound = self.find(str, 0, 1);
-			while (self.find(str, 0, 1))
-				continue;
-		}
-	} else if (navigator.appName.indexOf("Microsoft") != -1) {
-
-		// EXPLORER-SPECIFIC CODE
-
-		if (TRange != null) {
-			TRange.collapse(false);
-			strFound = TRange.findText(str);
-			if (strFound)
-				TRange.select();
-		}
-		if (TRange == null || strFound == 0) {
-			TRange = self.document.body.createTextRange();
-			strFound = TRange.findText(str);
-			if (strFound)
-				TRange.select();
-		}
-	} else if (navigator.appName == "Opera") {
-		alert("Opera browsers not supported, sorry...")
+function FindNext() {
+	var str = document.getElementById("findField").value;
+	if (str == "") {
+		alert("Please enter some text to search!");
 		return;
 	}
-	if (!strFound)
-		alert("String '" + str + "' not found!")
-	return;
+
+	var supported = false;
+	var found = false;
+	if (window.find) { // Firefox, Google Chrome, Safari
+		supported = true;
+		// if some content is selected, the start position of the search
+		// will be the end position of the selection
+		found = window.find(str);
+	} else {
+		if (document.selection && document.selection.createRange) { // Internet
+																	// Explorer,
+																	// Opera
+																	// before
+																	// version
+																	// 10.5
+			var textRange = document.selection.createRange();
+			if (textRange.findText) { // Internet Explorer
+				supported = true;
+				// if some content is selected, the start position of the search
+				// will be the position after the start position of the
+				// selection
+				if (textRange.text.length > 0) {
+					textRange.collapse(true);
+					textRange.move("character", 1);
+				}
+
+				found = textRange.findText(str);
+				if (found) {
+					textRange.select();
+				}
+			}
+		}
+	}
+
+	if (supported) {
+		if (!found) {
+			alert("The following text was not found:\n" + str);
+		}
+	} else {
+		alert("Your browser does not support this example!");
+	}
 }
